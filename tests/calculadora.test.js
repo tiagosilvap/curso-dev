b77bcc9ee24c07959e3521d2bb901e84bc157ff7
Tiago Silva <tiagosp13@gmail.com>
2026-02-09 22:10:57 -0300

    Add first unit test

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,6 @@
-test("Verifica se a soma de dois números é igual a 3", () => {
-  expect(1 + 2).toBe(3);
-});
+const calculadora = require('../models/calculadora.js');
+
+test('Soma entre 5 + 2 deve retornar 7', () => {
+  const resultado = calculadora.somar(5, 2);
+  expect(resultado).toBe(7);
+})
